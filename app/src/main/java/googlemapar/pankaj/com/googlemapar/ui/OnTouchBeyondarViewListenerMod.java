94ae5c277e01ba936a731ab9e2595619501c7528
pankajj <pankaj0619@gmail.com>
2018-06-01 15:48:43 +0530

    Initial commit

---
+++ app/src/main/java/googlemapar/pankaj/com/googlemapar/ui/OnTouchBeyondarViewListenerMod.java
@@ -0,0 +1,10 @@
+package googlemapar.pankaj.com.googlemapar.ui;
+
+import android.view.MotionEvent;
+
+/**
+ * Created by Pankaj Jangid on 06-01-2018.
+ */
+public interface OnTouchBeyondarViewListenerMod {
+    void onTouchBeyondarView(MotionEvent var1, ArBeyondarGLSurfaceView var2);
+}
