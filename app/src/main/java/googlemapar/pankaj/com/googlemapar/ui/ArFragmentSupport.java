1568cc669b00b9f6c050400d58b856bb5f851990
pankajj <pankaj0619@gmail.com>
2018-06-01 17:07:40 +0530

    Resolve Crash Bugs

--- app/src/main/java/googlemapar/pankaj/com/googlemapar/ui/ArFragmentSupport.java
+++ app/src/main/java/googlemapar/pankaj/com/googlemapar/ui/ArFragmentSupport.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -185,7 +186,7 @@ public class ArFragmentSupport extends Fragment implements FpsUpdatable,OnClickL
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         init();
         startRenderingAR();
         return mMainLayout;
